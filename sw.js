ce2801ee824039b3dd8aa2ce21c733af3795fc02
Serial Incubator <274148479+SerialIncubator@users.noreply.github.com>
2026-05-13 12:11:08 +1000

    feat: redesign profile screen with grouped sections and stats

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'training-log-v32';
+const CACHE = 'training-log-v33';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192px.png', './icon-512px.png'];
 
 self.addEventListener('install', e => {
