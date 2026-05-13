051ff9796ecc3a88cdd019815590db52d29c0094
Serial Incubator <274148479+SerialIncubator@users.noreply.github.com>
2026-05-13 12:23:15 +1000

    fix: dark mode Today card muted text and track now theme-aware

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'training-log-v33';
+const CACHE = 'training-log-v34';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192px.png', './icon-512px.png'];
 
 self.addEventListener('install', e => {
