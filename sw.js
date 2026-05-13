4d6f863955f81c1b646eccde956b35923ce8b684
Serial Incubator <274148479+SerialIncubator@users.noreply.github.com>
2026-05-13 12:05:07 +1000

    feat: tap logged sets to edit or delete, live Stripe links confirmed

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'training-log-v30';
+const CACHE = 'training-log-v31';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192px.png', './icon-512px.png'];
 
 self.addEventListener('install', e => {
